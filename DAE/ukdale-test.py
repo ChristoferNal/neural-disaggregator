1ca59c83c3e6e45634c9c5e27f95ad15b187c5e0
Christoforos <christofernal@gmail.com>
2017-09-15 18:24:24 +0300

    running experiments

--- DAE/ukdale-test.py
+++ DAE/ukdale-test.py
@@ -17,7 +17,7 @@ test.set_window(start="10-9-2013", end="10-10-2013")
 
 train_building = 2
 test_building = 2
-sample_period = 6
+sample_period = 1
 meter_key = 'fridge'
 train_elec = train.buildings[train_building].elec
 test_elec = test.buildings[test_building].elec
@@ -32,13 +32,15 @@ dae = DAEDisaggregator(300, True)
 start = time.time()
 print("========== TRAIN ============")
 epochs = 0
-for i in range(3):
-    print("CHECKPOINT {}".format(epochs))
-    dae.train(train_mains, train_meter, epochs=5, sample_period=sample_period)
-    epochs += 5
-    dae.export_model("UKDALE-DAE-h{}-{}-{}epochs.h5".format(train_building,
-                                                        meter_key,
-                                                        epochs))
+dae.train(train_mains, train_meter, epochs=25, sample_period=sample_period)
+
+# for i in range(3):
+#     print("CHECKPOINT {}".format(epochs))
+#     dae.train(train_mains, train_meter, epochs=10, sample_period=sample_period)
+#     epochs += 5
+#     dae.export_model("UKDALE-DAE-h{}-{}-{}epochs.h5".format(train_building,
+#                                                         meter_key,
+#                                                         epochs))
 end = time.time()
 print("Train =", end-start, "seconds.")
 
