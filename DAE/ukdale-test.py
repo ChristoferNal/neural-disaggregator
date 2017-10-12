fdfec1d90bb49fc551fd6f920524d476fa3b7717
Christoforos <christofernal@gmail.com>
2017-10-12 15:19:26 +0300

    commit for remote work

--- DAE/ukdale-test.py
+++ DAE/ukdale-test.py
@@ -18,17 +18,7 @@ test.set_window(start="10-9-2013", end="10-10-2013")
 train_building = 2
 test_building = 2
 sample_period = 1
-sample_period = 6
-meter_key = 'fridge'
-
-# train.set_window(start="7-7-2015", end="7-12-2015")
-# test.set_window(start="7-12-2015", end="2-2-2016")
-
-
-train_building = 2
-test_building = 2
-sample_period = 1
-meter_key = 'fridge'
+meter_key = 'laptop computer'
 with_embeddings = True
 epochs = 30
 
@@ -39,17 +29,6 @@ train_meter = train_elec.submeters()[meter_key]
 test_meter = test_elec.submeters()[meter_key]
 train_mains = train_elec.mains()
 test_mains = test_elec.mains()
-dae = DAEDisaggregator(300, True)
-
-
-start = time.time()
-print("========== TRAIN ============")
-epochs = 0
-dae.train(train_mains, train_meter, epochs=25, sample_period=sample_period)
-
-# for i in range(3):
-#     print("CHECKPOINT {}".format(epochs))
-#     dae.train(train_mains, train_meter, epochs=10, sample_period=sample_period)
 
 print('---------------------------------------------------------------------------')
 print("train_elec")
