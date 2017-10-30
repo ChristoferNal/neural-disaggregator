f6eb8f448c6ba3532263911bd5f6480193927a20
Christofer <christofernal@gmail.com>
2017-10-31 01:42:21 +0200

    setup experiments

--- run_experiments.py
+++ run_experiments.py
@@ -9,31 +9,30 @@ UK_DALE = '../Datasets/UKDALE/ukdale.h5'
 UK_DALE_NAME = "ukdale"
 REDD_NAME = "redd"
 
-use_embeddings = True
+use_embeddings = False
 dae = DAEDisaggregator(300, use_embeddings)
 exp_uk_fridge = Experiment(train_dataset_name=UK_DALE_NAME,
                                 name="DAE",
                                 disaggregator=dae,
                                 train_dataset_path=UK_DALE,
-                                train_building=2,
+                                train_building=1,
                                 start="20-5-2013",
-                                end="9-9-2013",
-                                sample_period=1,
+                                end="31-12-2014",
+                                sample_period=128,
                                 device='fridge',
                                 with_embeddings=use_embeddings,
-                                epochs=1)
+                                epochs=30)
 exp_uk_fridge.train_model()
 exp_uk_fridge.set_test_params(test_dataset_path=UK_DALE, test_dataset_name=UK_DALE_NAME,
-                              test_building=2)
-exp_uk_fridge.set_testing_window(start="10-9-2013", end="10-10-2013")
+                              test_building=1)
+exp_uk_fridge.set_testing_window(start="1-1-2015", end="31-12-2015")
 exp_uk_fridge.run_experiment()
 
 exp_uk_fridge.set_test_params(test_dataset_path=UK_DALE, test_dataset_name=UK_DALE_NAME,
-                              test_building=1)
-exp_uk_fridge.set_testing_window(start="10-9-2013", end="10-10-2013")
+                              test_building=2)
+exp_uk_fridge.set_testing_window(start=None, end=None)
 exp_uk_fridge.run_experiment()
 
 exp_uk_fridge.set_test_params(test_dataset_path=REDD, test_dataset_name=REDD_NAME,
                               test_building=1)
-exp_uk_fridge.set_testing_window(start="30-4-2011")
 exp_uk_fridge.run_experiment()
